4516299ac24531b29a17c1dfddbf668177563ccf
WashingtonA01 <arianna.s.washington@gmail.com>
2025-02-12 08:27:09 -0500

    Add project files.

---
+++ Workbench Problem 25.cpp
@@ -0,0 +1,35 @@
+/*
+* Filename:Workbenchproblem25.cpp
+* Programmer: Arianna Washington
+* Date:12FEB2025
+* Requirements: A retail store grants its customers a maximum amount of credit. Each customer's available credit
+* is their mazimum amount of credut minus the amount of credit used. Write an algorithm and then a C ++ program that asks
+* for a customer's maximum amount of credit, and amount of credit used. Display all information.
+
+1. Declare our variables.
+2. Get the variable information from the user. 
+3. Calculate.
+4. Display input and computed information.
+*/
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+	double maxCredit; 
+	double creditused;
+	
+	cout << "What is the maximum credit on your store card?" << endl;
+	cin >> maxCredit;
+	cout << "How much credit have you used already?" << endl;
+	cin >> creditused;
+
+	double availableCredit = maxCredit - creditused;
+
+	cout << "Your available credit is: " << availableCredit;
+
+
+  
+}
+
